0b2ddd4277f2d3ced5d52e67e1bb1762da223ba9
outerskyb <outerskyb@gmail.com>
2021-02-08 01:20:51 +0900

    fix multiple devices problem

--- models/morn.py
+++ models/morn.py
@@ -110,7 +110,10 @@ class MORN(nn.Module):
 
                 v_max = offsets_max.data[i]
                 v_min = offsets_min.data[i]
-                img_offsets = (offsets_grid[i]).view(1, self.targetH, self.targetW).data.cpu().add_(-v_min).mul_(1./(v_max-v_min))
+                 if self.cuda:
+                    img_offsets = (offsets_grid[i]).view(1, self.targetH, self.targetW).data.cuda().add_(-v_min).mul_(1./(v_max-v_min))
+                else:
+                    img_offsets = (offsets_grid[i]).view(1, self.targetH, self.targetW).data.cpu().add_(-v_min).mul_(1./(v_max-v_min))
                 img_offsets = to_pil_image(img_offsets)
                 img_offsets = np.array(img_offsets)
                 color_map = np.empty([self.targetH, self.targetW, 3], dtype=int)
